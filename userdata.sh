8423d673412739f7926a3d4daa5d62ac2249e3de
vasudev <103812191+vasudev915@users.noreply.github.com>
2023-03-03 10:29:06 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 sudo yum update
 sudo yum -y install git
-sudo amazon-linux-extras install docker
-sudo yum install docker
+sudo amazon-linux-extras install docker -y
+sudo yum install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
