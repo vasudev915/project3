d7e66eceecd54c44b987b6a4602727ccce81c171
vasudev <103812191+vasudev915@users.noreply.github.com>
2023-03-07 11:19:08 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -8,6 +8,4 @@ sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-git clone https://github.com/vasudev915/wordpress-db-yaml.git
-cd wordpress-db-yaml/
 docker-compose up -d
