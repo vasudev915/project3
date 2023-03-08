59ddd9e924804e263f220467063caabc3e011c29
vasudev <103812191+vasudev915@users.noreply.github.com>
2023-03-08 09:56:06 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,12 +3,15 @@ sudo yum update
 sudo yum -y install git
 sudo amazon-linux-extras install docker -y
 sudo yum install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 mkdir git
-git clone https://github.com/vasudev915/wordpress-db-yaml.git /home/ec2-user/git/
-cd wordpress-db-yaml
+sudo chmod 666 /var/run/docker.sock
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose up -d
+git clone https://github.com/vasudev915/wordpress-db-yaml.git /home/ec2-user/git/
+cd git/
+sudo docker-compose up -d
