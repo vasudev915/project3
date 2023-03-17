1d7b5eee1542e163aa20fc8ee66f278c2118079b
vasudev <103812191+vasudev915@users.noreply.github.com>
2023-03-17 09:45:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,6 +3,6 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAU5E5PQTN4SR2QWRW"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
